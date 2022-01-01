dc0f567b8b80a92712c608f9cf415cd8f53564e3
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-01 17:31:52 +0300

    test: add tests for `<ZoomControl />`

---
+++ src/components/Map/controls/ZoomControl/ZoomControl.test.tsx
@@ -0,0 +1,87 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+import { Map as YMap } from 'yandex-maps'
+import MapContext from '../../MapContext'
+import ZoomControl from './ZoomControl'
+
+it('should render both + and - controls', () => {
+  const tree = renderer
+    .create(
+      <MapContext.Provider value={{} as YMap}>
+        <ZoomControl />
+      </MapContext.Provider>
+    )
+    .toJSON()
+
+  expect(tree).toMatchSnapshot()
+})
+
+it(`should throw error when map isn't provided`, () => {
+  expect(() => render(<ZoomControl />)).toThrow(
+    new Error('[Map] Map should be created before it usage')
+  )
+})
+
+it('should increase zoom when + is clicked', () => {
+  let currentZoom = 9
+
+  const mockMap = {
+    getZoom: jest.fn(() => currentZoom),
+    setZoom: jest.fn((zoom: number) => {
+      currentZoom = zoom
+      return Promise.resolve()
+    }),
+  } as unknown as YMap
+
+  render(
+    <MapContext.Provider value={mockMap}>
+      <ZoomControl />
+    </MapContext.Provider>
+  )
+
+  fireEvent.click(screen.getByText('+'))
+
+  expect(mockMap.getZoom).toHaveBeenCalledTimes(1)
+  expect(mockMap.setZoom).toHaveBeenCalledTimes(1)
+  expect(mockMap.setZoom).toHaveBeenCalledWith(10, { duration: 300 })
+
+  fireEvent.click(screen.getByText('+'))
+  fireEvent.click(screen.getByText('+'))
+
+  expect(mockMap.getZoom).toHaveBeenCalledTimes(3)
+  expect(mockMap.setZoom).toHaveBeenCalledTimes(3)
+  expect(mockMap.setZoom).toHaveBeenCalledWith(12, { duration: 300 })
+  expect(mockMap.setZoom).toHaveBeenCalledWith(12, { duration: 300 })
+})
+
+it('should decrease zoom when - is clicked', () => {
+  let currentZoom = 9
+
+  const mockMap = {
+    getZoom: jest.fn(() => currentZoom),
+    setZoom: jest.fn((zoom: number) => {
+      currentZoom = zoom
+      return Promise.resolve()
+    }),
+  } as unknown as YMap
+
+  render(
+    <MapContext.Provider value={mockMap}>
+      <ZoomControl />
+    </MapContext.Provider>
+  )
+
+  fireEvent.click(screen.getByText('-'))
+
+  expect(mockMap.getZoom).toHaveBeenCalledTimes(1)
+  expect(mockMap.setZoom).toHaveBeenCalledTimes(1)
+  expect(mockMap.setZoom).toHaveBeenCalledWith(8, { duration: 300 })
+
+  fireEvent.click(screen.getByText('-'))
+  fireEvent.click(screen.getByText('-'))
+
+  expect(mockMap.getZoom).toHaveBeenCalledTimes(3)
+  expect(mockMap.setZoom).toHaveBeenCalledTimes(3)
+  expect(mockMap.setZoom).toHaveBeenCalledWith(7, { duration: 300 })
+  expect(mockMap.setZoom).toHaveBeenCalledWith(6, { duration: 300 })
+})
