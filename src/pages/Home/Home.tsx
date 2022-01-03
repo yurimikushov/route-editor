dfece7dc9728e839032b1141e2e05d70140c4ff1
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-03 09:27:34 +0300

    fix: correct margins

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,7 +7,7 @@ import HomeProps from './Home.props'
 const Home: FC<HomeProps> = ({ className }) => {
   return (
     <div className={cn(className, 'flex')}>
-      <SearchForm className='p-3' />
+      <SearchForm className='m-3' />
       <Map className='relative w-full h-screen bg-gray-50'>
         <ZoomControl className='absolute top-1/2 right-4 transform -translate-y-1/2' />
       </Map>
