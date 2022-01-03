c02fc33296f46c57b799000343cdcdc687035653
Yuri Mikushov <mikushov.yu@gmail.com>
2022-01-03 13:35:11 +0300

    style: make naming clearly

--- src/components/Map/geoObjects/Placemark/Placemark.props.ts
+++ src/components/Map/geoObjects/Placemark/Placemark.props.ts
@@ -6,7 +6,7 @@ type PlacemarkProps = {
   preset?: 'islands#blueCircleIcon'
   balloonContent?: string
   draggable?: boolean
-  onDragEnd?: (coords: { lat: number; lon: number }) => void
+  onDragEnd?: (point: { lat: number; lon: number }) => void
 }
 
 export default PlacemarkProps
